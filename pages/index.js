2839f498488511e707ed0b515fef3ecd75851159
Ganesh <ganesh@cleardigital.com>
2025-04-18 23:05:07 +0530

    update svg

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import Tools from "@/components/Tools";
 import ContactUs from "@/components/ContactUs";
 import Image from "next/image";
 
-
 export default function Index() {
   return (
     <>
@@ -18,8 +17,28 @@ export default function Index() {
               <div className="leftContent w-[30%]">
                 <div
                   data-aos="fade-up"
-                  className="stickyCard sticky bg-white  top-[50px] px-[20px] py-[30px] shadow-lg text-center rounded-lg"
+                  className="stickyCard sticky bg-white  top-[50px] px-[20px] py-[30px] shadow-lg text-center rounded-lg overflow-hidden"
                 >
+                  <div className="absolute -top-[150px] -left-[150px] w-auto h-auto">
+                    <svg
+                      width="300"
+                      height="300"
+                      viewBox="0 0 300 300"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle
+                        cx="150"
+                        cy="150"
+                        r="120"
+                        fill="none"
+                        stroke="#ff7a00"
+                        strokeWidth="4"
+                        strokeDasharray="7 10" // Bigger dots & spacing
+                        strokeLinecap="square"
+                      />
+                    </svg>
+                  </div>
+
                   <div className="max-w-[240px] max-h-[284px] w-full h-full rounded-lg overflow-hidden mx-auto mb-[20px]">
                     <Image
                       width={1000}
@@ -30,6 +49,38 @@ export default function Index() {
                     />
                   </div>
                   <h3 className="text-[36px] font-semibold">Ganesh Thakur</h3>
+                  <div className=" rotate-[355deg] relative left-[-100px]">
+                    <svg
+                      width="200"
+                      height="130"
+                      viewBox="0 0 300 150"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="half-dotted-circle"
+                    >
+                      <path
+                        d="M 30 0 A 120 120 0 0 0 270 0"
+                        fill="none"
+                        stroke="#ff7a00"
+                        strokeWidth="4"
+                        strokeDasharray="4 10"
+                        strokeLinecap="square"
+                      />
+
+                      <circle cx="270" cy="0" r="17.5" fill="#ff7a00" />
+
+                      <text
+                        x="270"
+                        y="5"
+                        textAnchor="middle"
+                        fill="white"
+                        fontSize="12"
+                        fontWeight="bold"
+                        fontFamily="monospace"
+                      >
+                        {"< / >"}
+                      </text>
+                    </svg>
+                  </div>
                   <p className="text-gray-600 mt-2">
                     Turning ideas into sleek, high-performance web experiences.
                   </p>
